96590ae43eadf82945b20426ca905c30ca1ae857
sricharanmohankumar <S.MohanKumar001@umb.edu>
2023-05-14 12:50:47 -0400

    Adding comments to main code

--- project682/main.py
+++ project682/main.py
@@ -10,8 +10,9 @@ from lib.third_party import rsa
 from ubinascii import b2a_base64
 from lib.third_party import string
 from aes import aes_decrypt
-import Micropython_Test 
+import Micropython_Test
 
+#initializing variables
 ssid = password = atSign = ''
 aesEncryptPrivateKey = aesEncryptPublicKey = aesPkamPrivateKey = aesPkamPublicKey = selfEncryptionKey = ''
 privateKey = []
@@ -20,12 +21,15 @@ sensor_temp = machine.ADC(4)
 conversion_factor = 3.3 / (65535)
 
 def main():
-    
+    #initializing variables
     global ssid, password, atSign, privateKey, aesEncryptPrivateKey, aesEncryptPublicKey, aesPkamPrivateKey, aesPkamPublicKey, selfEncryptionKey
+    #stores the data read from settings.json
     ssid, password, atSign, privateKey = read_settings()
+    #stores the data read from keys file
     aesEncryptPrivateKey, aesEncryptPublicKey, aesPkamPrivateKey, aesPkamPublicKey, selfEncryptionKey = read_key(atSign)
     pkamPrivateKey = aes_decrypt(aesPkamPrivateKey, selfEncryptionKey)
 
+    # connecting to Wi-Fi
     wlan = network.WLAN(network.STA_IF)  # type: ignore
     wlan.active(True)
     wlan.connect(ssid, password)
@@ -35,9 +39,10 @@ def main():
         print("Wi-Fi .. Connecting")
 
     print ("Wi-Fi Connected")
-
+    # syncs the time with an NTP server
     sync_time()
     
+    # displays the main menu
     while True:
         print("Welcome! What would you like to do?\n\t1) REPL\n\t2) REPL (from:atSign challenge is completed automatically)\n\t3) Get privateKey for @" + atSign + "\n\t4) Temperature sensor menu\n\t5) Test\n\t6) Exit")
         opt= input("> ")
@@ -51,6 +56,7 @@ def main():
             command = '@'
             
             if int(opt) == 2:
+                 # automatically completes the challenge from the secondary
                 response, command = send_verb(ss, 'from:' + atSign)
                 if 'data:_' in response:
                     print("Signing challenge... (This process can take up to 1 minute)")
@@ -65,6 +71,7 @@ def main():
                     sys.exit(1)
             
             while True:
+                # waits for user input and sends it to the secondary
                 verb = str(input(command))
                 response, command = send_verbs(ss, verb)
                 print("resp :"+response)
@@ -73,6 +80,7 @@ def main():
                     sys.exit(1)
                     
         elif int(opt) == 3:
+            # generates a new private key and saves it to settings.json
             from pem_service import get_pem_parameters, get_pem_key
             
             pemKey = get_pem_key(pkamPrivateKey)
@@ -120,8 +128,10 @@ def main():
                     print('Invalid option. Please enter a number in the range [1-3]')
             
         elif int(opt) == 6:
+            #terminates the program
             sys.exit(1)
         elif int(opt) == 5:
+            #run the test cases on this file
             print('Running tests...')
             test_case = Micropython_Test.TestMicropythonCode()
             test_case.test_find_secondary()
@@ -131,10 +141,12 @@ def main():
             sys.exit()
         else:
             print('Invalid option. Please enter a number in the range [1-4]')
-
+#send a verb to a socket and return the response
 def send_verbs(skt, verb):
+    # Print debug information
     print('skt: '+str(skt))
     print('verb: '+verb)
+    # Send the verb to the socket and wait for a response
     skt.write((verb + "\r\n").encode())
     print('skt :' + str(skt))
     print('verb :'+ verb)
@@ -142,9 +154,11 @@ def send_verbs(skt, verb):
     response = b''
     data = skt.read()
     response += data
+    # Split the response into separate parts
     parts = response.decode().split('\n')
     return parts[0], parts[1]
     
+# sending a single verb to a socket and return the response
 def send_verb(skt, verb):
     skt.write((verb + "\r\n").encode())
     time.sleep(1)
@@ -152,39 +166,46 @@ def send_verb(skt, verb):
     data = skt.read()
     response += data
     parts = response.decode().split('\n')
-    # 
+    # If the verb is a llookup, decrypt the first part of the response
     if 'llookup:' in verb:
         content = parts[0].replace('data:', '')
         parts[0] = aes_decrypt(content, selfEncryptionKey)
         
     return parts[0], parts[1]
     
+# Read the raw ADC value from the temperature sensor and convert it to a temperature value
 def measure_temp():
     reading = sensor_temp.read_u16() * conversion_factor 
     temperature = 27 - (reading - 0.706)/0.001721
     return temperature
-
+# Open the settings file and load the JSON data
 def read_settings():
     with open('settings.json') as f:
         info = ujson.loads(f.read())
         return info['ssid'], info['password'], info['atSign'].replace('@', ''), info['privateKey']
     
 def read_key(atSign):
+    # Build the path to the key file
     path = '/keys/@' + atSign + '_key.atKeys'
+    # Open the key file and load the JSON data
     with open(path) as f:
         info = ujson.loads(f.read())
+        # Return the encryption keys
         return info['aesEncryptPrivateKey'], info['aesEncryptPublicKey'], info['aesPkamPrivateKey'], info['aesPkamPublicKey'], info['selfEncryptionKey']
 
+#function to connect to a secondary server
 def connect_to_secondary(secondary):
+    # Extract the address and port number from the secondary server information
     print('Connecting to secondary... ', end="")
 
     secondary = secondary.split(':')
     address = secondary[0]
     port = secondary[1]
-
+    # Resolve the IP address of the secondary server
     a = socket.getaddrinfo(address, int(port))[0][-1]
+    # Create a TCP socket and connect to the secondary server
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-
+    # try connecting to secondary
     try:
         s.connect(a)
     except OSError as e:
@@ -192,16 +213,17 @@ def connect_to_secondary(secondary):
             print("In Progress")
         else:
             raise e
-        
+    # Set the socket to non-blocking and wrap it with SSL encryption.    
     s.setblocking(False)
     ss = ssl.wrap_socket(s, do_handshake = True)
     return ss
-    
+#function to find secondary server    
 def find_secondary(atSign):
     print('Finding secondary for @' + atSign + '...')
+    # Get the server address.
     a = socket.getaddrinfo('root.atsign.org', 64)[0][-1]
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-
+    # try connecting to secondary
     try:
         s.connect(a)
     except OSError as e:
@@ -210,18 +232,22 @@ def find_secondary(atSign):
             print("In Progress")
         else:
             raise e
+    # Set the socket to non-blocking and wrap it with SSL encryption.    
     s.setblocking(False)
     ss = ssl.wrap_socket(s, do_handshake = True)
 
+    # Write the 'atSign' to the socket and wait for a response.
     ss.write((atSign + "\r\n").encode())
     time.sleep(1)
 
+    # Extract the secondary address from the response and return it.
     response = b''
     data = ss.read()
     response += data
     secondary = response.decode().replace('@', '')
     secondary = secondary.replace('\r\n', '')
  
+    # Close the socket and print the secondary address.
     ss.close()
     print('Address found: %s' % secondary)
     return secondary
